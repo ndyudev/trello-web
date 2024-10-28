292c08c2439f6656d2ecce37e4d809e05608e141
Chauu Nhat Duyy ( Dyu ) <119422970+ChauuNhatDuyyIT@users.noreply.github.com>
2024-10-28 23:43:05 +0700

    app_bar_p2

--- src/theme.js
+++ src/theme.js
@@ -1,27 +1,60 @@
 import { experimental_extendTheme as extendTheme } from '@mui/material/styles'
 import { deepOrange, orange, cyan, teal } from '@mui/material/colors'
 
-//Create a theme instance.
+// Create a theme instance.
 const theme = extendTheme({
-    trello: {
-        AppBarHeight:'58px',
-        BoardBarHeight:'60px'
+  trello: {
+    AppBarHeight: '58px',
+    BoardBarHeight: '60px',
+  },
+  colorSchemes: {
+    light: {
+      palette: {
+        primary: teal,
+        secondary: deepOrange,
+      },
     },
-    colorSchemes: {
-        light: {
-            palette: {
-                primary: teal,
-                secondary: deepOrange,
-            },
+    dark: {
+      palette: {
+        primary: cyan,
+        secondary: orange,
+      },
+    },
+  },
+  components: {
+    MuiButton: {
+      styleOverrides: {
+        root: {
+          textTransform: 'none',
         },
-        dark: {
-            palette: {
-                primary: cyan,
-                secondary: orange
-            },
-        }
-    }
-    // ...other properties
+      },
+    },
+    MuiInputLabel: {
+      styleOverrides: {
+        root: ({ theme }) => ({
+          color: theme.palette.primary.main,
+          fontSize: '0.785rem', // Xóa khoảng trắng thừa
+        }),
+      },
+    },
+    MuiOutlinedInput: {
+      styleOverrides: {
+        root: ({ theme }) => ({
+          color: theme.palette.primary.main,
+          fontSize: '0.785rem',
+          '.MuiOutlinedInput-notchedOutline': {
+            borderColor: theme.palette.primary.light,
+          },
+          '&:hover .MuiOutlinedInput-notchedOutline': {
+            borderColor: theme.palette.primary.main,
+          },
+          '& fieldset': {
+            borderWidth: '1px !important',
+          },
+        }),
+      },
+    },
+  },
 })
 
 export default theme
